f6e53a08ede02bbdd83e21e7625d5adfc1aa6db5
colin03ttr <colin.tanter@edu.devinci.fr>
2025-03-10 16:06:54 +0100

    finished

--- solve.py
+++ solve.py
@@ -1,21 +1,23 @@
 import base64
+import string
 import subprocess
 
 # Connect to the server and read the output directly
 def get_cipher(payload):
-    result = subprocess.run(f'echo {payload} | nc localhost 9999', capture_output=True, text=True, shell=True)
+    result = subprocess.run(f'echo \'{payload}\' | nc localhost 9999', capture_output=True, text=True, shell=True)
     output = result.stdout.strip().split('\n')
     line = output[-1] if len(output) > 1 else ''
     words = line.split(': ')
     cipher_base64 = words[-1] if len(words) > 1 else ''
     #print(f"Payload: {payload}, Cipher: {cipher_base64}")
+    cipher_base64 = cipher_base64.encode('ascii')
     cipher_bytes = base64.b64decode(cipher_base64)
     return cipher_bytes
 
 def bruteforce():
     flag = ''
     total = 16 - 1
-    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-~!?#%&@{}'
+    chars = string.printable
 
     while True:
         payload = '1' * (total - len(flag))
@@ -25,8 +27,8 @@ def bruteforce():
         for char in chars:
             payload = '1' * (total - len(flag)) + flag + char
             cipher = get_cipher(payload)
-            #print(f"block_target: {cipher_target[32:64]}, block: {cipher[32:64]}")
-            if cipher[32:64] == cipher_target[32:64]:
+            #print(f"block_target: {cipher_target[0:32]}, block: {cipher[0:32]}")
+            if cipher[0:32] == cipher_target[0:32]:
                 flag += char
                 break
 
@@ -39,3 +41,4 @@ def bruteforce():
             break
 
 bruteforce()
+
